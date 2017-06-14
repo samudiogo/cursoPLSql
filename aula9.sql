d04f73f6f233fe214923cd6e55dc6a609adf8bd8
Samuel Diogo <samudiogo@users.noreply.github.com>
2017-06-14 19:49:09 -0300

    criação da modelagem de dados

--- aula9.sql
+++ aula9.sql
@@ -19,3 +19,30 @@ startup;
 alter session set nls_date_format='dd/mm/yyyy';
 set serveroutput on size 10000;
 set long 10000;
+
+--------------------------------
+
+create table produtos(idProduto number(15) primary key,
+nome varchar(50),
+preco number(20,2));
+
+create sequence seq_produto;
+
+
+create table estoque (idEstoque number (20) primary key,
+ dataestoque date,
+ quantidade number (5),
+ id_produto number (15) unique,
+ foreign key(id_produto) references produtos);
+
+create sequence seq_estoque;
+
+create table venda (idVenda number (15) primary key,
+ datavenda timestamp, quantidadevendido number (15,2),
+  id_produto number (15),
+  valorVenda number (15,2),
+  foreign key(id_produto) references produtos
+  );
+
+create sequence seq_venda;
+
