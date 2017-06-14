a37651211a88b89d3e74826e273396e7c6cb772d
Samuel Diogo <samudiogo@users.noreply.github.com>
2017-06-14 19:47:19 -0300

    preparando ambiente para aula 9

---
+++ aula9.sql
@@ -0,0 +1,17 @@
+sqlplus system/coti
+
+create user sessao009 identified by coti
+ default tablespace users
+ quota 100m on users;
+ 
+
+grant create table, create session, create procedure,
+      create user,  create trigger, create sequence,
+	   create view, create materialized view  to sessao009;
+	   
+conn sys/coti as sysdba
+alter system set utl_file_dir='*'  scope=spfile;
+grant execute on utl_file to sessao009;
+
+shutdown immediate;
+startup;
