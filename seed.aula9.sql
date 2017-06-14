c9ca884142a29bc12c04be6be23112260e4f951a
Samuel Diogo <samudiogo@users.noreply.github.com>
2017-06-14 19:52:49 -0300

    criação de seed inicial

---
+++ seed.aula9.sql
@@ -0,0 +1,26 @@
+--- insert gerado automaticamente através do website http://www.mockaroo.com
+
+insert into produtos (idproduto, nome, preco) values (1, 'Ntags', 4176);
+insert into produtos (idproduto, nome, preco) values (2, 'Realpoint', 3305);
+insert into produtos (idproduto, nome, preco) values (3, 'Buzzshare', 2440);
+insert into produtos (idproduto, nome, preco) values (4, 'Twitterlist', 1497);
+insert into produtos (idproduto, nome, preco) values (5, 'Meevee', 4134);
+insert into produtos (idproduto, nome, preco) values (6, 'Realcube', 2427);
+insert into produtos (idproduto, nome, preco) values (7, 'Demizz', 3072);
+insert into produtos (idproduto, nome, preco) values (8, 'Tagcat', 1412);
+insert into produtos (idproduto, nome, preco) values (9, 'Devify', 4561);
+insert into produtos (idproduto, nome, preco) values (10, 'Yata', 1753);
+
+
+----
+
+insert into estoque (idestoque, dataestoque, quantidade, id_produto) values (1, '12/05/2017', 5, 1);
+insert into estoque (idestoque, dataestoque, quantidade, id_produto) values (2, '01/05/2017', 8, 2);
+insert into estoque (idestoque, dataestoque, quantidade, id_produto) values (3, '14/03/2017', 5, 3);
+insert into estoque (idestoque, dataestoque, quantidade, id_produto) values (4, '21/12/2016', 10, 4);
+insert into estoque (idestoque, dataestoque, quantidade, id_produto) values (5, '08/02/2017', 3, 5);
+insert into estoque (idestoque, dataestoque, quantidade, id_produto) values (6, '25/08/2016', 8, 6);
+insert into estoque (idestoque, dataestoque, quantidade, id_produto) values (7, '28/10/2016', 1, 7);
+insert into estoque (idestoque, dataestoque, quantidade, id_produto) values (8, '17/08/2016', 8, 8);
+insert into estoque (idestoque, dataestoque, quantidade, id_produto) values (9, '07/05/2017', 1, 9);
+insert into estoque (idestoque, dataestoque, quantidade, id_produto) values (10, '23/04/2017', 5, 10);
