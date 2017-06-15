ed596096708c0326fa5905cbc52cf5e0a6deb23c
Samuel Diogo <samudiogo@users.noreply.github.com>
2017-06-14 21:45:13 -0300

    trabalhando com datas

--- aula9.sql
+++ aula9.sql
@@ -198,3 +198,84 @@ end;
 /
 
 -------------------------------------------------
+declare
+   data_atual date;
+   data_atual_maisummes date;
+   data_atual_menosummes date;
+   ultimo_dia_mes  date;
+   primeiro_dia_mes  date;
+   qtde_mes number (15) :=0;
+   arredonda_abaixo number (15):=0;
+   arredonda_acima number (15):=0;
+   valor_absoluto  number (15):=0;
+   arredonda  number (15):=0;
+   primeiro_dia_ano date;
+   diaMes_extenso varchar(100);
+   Mes_extenso varchar(100);
+   hora_minuto varchar (35);
+   mes_corrente varchar (35);
+   dias_data date;
+   idade number (15);
+   dia_da_semana varchar (35);
+  begin
+    select sysdate into data_atual from dual;
+	select add_months(sysdate,-1) into data_atual_menosummes from dual;
+    select add_months(sysdate,+1) into data_atual_maisummes from dual;
+    select last_day(sysdate) into ultimo_dia_mes  from dual;
+	select trunc(sysdate,'MONTH') into primeiro_dia_mes  from dual;
+select months_between(sysdate,'01-JAN-2016') into qtde_mes  from dual;
+
+   select floor(months_between(sysdate,'01/06/2016')) into    arredonda_abaixo  from dual;
+	select ceil(months_between(sysdate,'28/06/2016')) into   arredonda_acima  from dual;
+	select ABS(months_between(sysdate,'14/06/2016')) into 
+    valor_absoluto  from dual;
+	
+  select round((months_between(sysdate,'01-JAN-2016')),2) into 
+    arredonda  from dual;
+	
+  select to_char(sysdate,'dd " de " FMMONTH " de " YYYY',
+   'nls_date_language=portuguese') into diaMES_extenso from dual;
+   
+  select to_char(sysdate,'FMMONTH " de " YYYY',
+   'nls_date_language=portuguese') into MES_extenso from dual;
+  
+  select trunc(sysdate,'year') into primeiro_dia_ano from dual;
+  select to_char(sysdate,'HH24:MI') into hora_minuto from dual;
+  select to_char(sysdate,'FMMONTH','nls_date_language=portuguese')
+  into mes_corrente from dual;
+----	
+ select to_date(lpad(to_char(1234567),4,'0'),'hh24mi') 
+  into dias_data from dual;
+-----
+
+
+ select floor(floor(months_between(sysdate,'28/01/1973'))/12)
+  into idade from dual;
+	
+ select decode (to_number(to_char(sysdate,'D')),1,'domingo',2,
+'segunda',3,'terca',4,'quarta',5,'quinta',6,'sexta',7,'sabado')
+ into dia_da_semana from dual;
+ 
+ dbms_output.put_line('Datas ...');
+ dbms_output.put_line('Data Atual' || data_atual);
+ dbms_output.put_line('Data Atual menos um mes :' || data_atual_menosummes);
+ dbms_output.put_line('Data Atual mais um mes :' || data_atual_maisummes);
+ dbms_output.put_line('Ultimo dia Mes :' || ultimo_dia_mes);
+ dbms_output.put_line('Primeiro dia :' || primeiro_dia_mes);
+ dbms_output.put_line('Quantidade Meses :' || qtde_mes);
+ dbms_output.put_line('Arredondado cima :' || arredonda_acima);
+ dbms_output.put_line('Arredondado Abaixo :' || arredonda_abaixo);
+ dbms_output.put_line('Valor absoluto :' || valor_absoluto);
+ dbms_output.put_line('Arredonda :' || arredonda);
+ dbms_output.put_line('Primeiro dia Ano :' || primeiro_dia_ano);
+ dbms_output.put_line('Dia Mes por extenso :' || diaMES_extenso);
+ dbms_output.put_line('Mes por extenso :' || MES_extenso);
+ dbms_output.put_line('Hora MInuto :' || hora_minuto);
+ dbms_output.put_line('MEs Corrente :' || mes_corrente);
+ dbms_output.put_line('Dias Data :' || dias_data);
+ dbms_output.put_line('Idade :' || idade);
+ dbms_output.put_line('dia da Semana :' || dia_da_semana);
+ 
+ exception when others then  
+   dbms_output.put_line('Error ' || sqlerrm);
+ end;
